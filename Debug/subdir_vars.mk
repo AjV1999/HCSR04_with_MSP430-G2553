2dabb06ce4080a967e781ec830ad10d31949a9f8
AjV1999 <57947483+AjV1999@users.noreply.github.com>
2021-06-03 16:46:19 +0530

    Add files via upload

---
+++ Debug/subdir_vars.mk
@@ -0,0 +1,35 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430g2553.cmd 
+
+C_SRCS += \
+../main.c \
+../ultra_source.c 
+
+C_DEPS += \
+./main.d \
+./ultra_source.d 
+
+OBJS += \
+./main.obj \
+./ultra_source.obj 
+
+OBJS__QUOTED += \
+"main.obj" \
+"ultra_source.obj" 
+
+C_DEPS__QUOTED += \
+"main.d" \
+"ultra_source.d" 
+
+C_SRCS__QUOTED += \
+"../main.c" \
+"../ultra_source.c" 
+
+
---
+++ Debug/ultra_source.d
@@ -0,0 +1,15 @@
+# FIXED
+
+ultra_source.obj: ../ultra_source.c
+ultra_source.obj: ../lcd_ultra.h
+ultra_source.obj: C:/ti/ccs910/ccs/ccs_base/msp430/include/msp430g2553.h
+ultra_source.obj: C:/ti/ccs910/ccs/ccs_base/msp430/include/in430.h
+ultra_source.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-msp430_18.12.3.LTS/include/intrinsics.h
+ultra_source.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-msp430_18.12.3.LTS/include/intrinsics_legacy_undefs.h
+
+../ultra_source.c: 
+../lcd_ultra.h: 
+C:/ti/ccs910/ccs/ccs_base/msp430/include/msp430g2553.h: 
+C:/ti/ccs910/ccs/ccs_base/msp430/include/in430.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-msp430_18.12.3.LTS/include/intrinsics.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-msp430_18.12.3.LTS/include/intrinsics_legacy_undefs.h: 
